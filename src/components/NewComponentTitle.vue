08bae5cf105411e4397c800aa7d322b76a403d40
katiepalumbo <palumbo.katie@gmail.com>
2022-02-21 10:06:45 +0100

    esercizio completato

---
+++ src/components/NewComponentTitle.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vue CLI!</h1>
+        <h3> Woohoo! </h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NewComponentTitle'
+    }
+</script>
+
+<style>
+
+</style>
